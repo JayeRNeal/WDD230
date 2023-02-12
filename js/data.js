fc751a98685cc84d89b827228cc1c24f3560f758
Use your GitHub username <103981244+JayeRNeal@users.noreply.github.com>
2023-02-11 22:09:53 -0700

    index

--- js/data.js
+++
@@ -1,6 +0,0 @@
-var text = document.lastModified;
-document.getElementById("demo").innerHTML = text;
-
-document.getElementById("year").innerHTML = (new Date().getFullYear());
-
-
--- start-navigation/js/scripts.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu(){
-    document.getElementById("primaryNav").classList.toggle("open");
-    document.getElementById("hamburgerBtn").classList.toggle("open");
-
-}/*toggle open means that the hamburger menu will open and close when you click
-on it*/
-
-const x = document.getElementById("hamburgerBtn")
-x.onclick= toggleMenu;
